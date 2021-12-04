1b8daa9009c33e21f40a15b98268b0d7689ddc0e
Oleksii Shuliev <alexey.shulev@gmail.com>
2021-12-04 09:53:51 +0100

    day 4

---
+++ day4/day4.go
@@ -0,0 +1,119 @@
+package day4
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const size = 5
+
+type board struct {
+	rowSums, colSums []int
+	nums             [][]int
+}
+
+func (b *board) mark(num int) bool {
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if b.nums[i][j] == num {
+				b.nums[i][j] = -1
+				b.rowSums[i]++
+				b.colSums[j]++
+				if b.rowSums[i] == size || b.colSums[j] == size {
+					return true
+				}
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func (b *board) sum() int {
+	var sum int
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			if b.nums[i][j] > 0 {
+				sum += b.nums[i][j]
+			}
+		}
+	}
+	return sum
+}
+
+func createBoard() *board {
+	b := board{
+		nums:    make([][]int, 0, size),
+		rowSums: make([]int, size),
+		colSums: make([]int, size),
+	}
+	return &b
+}
+
+func getNumLine(str, sep string) []int {
+	space := regexp.MustCompile(`\s+`)
+	s := strings.Trim(space.ReplaceAllString(str, " "), " ")
+	numsLine := strings.Split(s, sep)
+	nums := make([]int, 0, len(numsLine))
+	for _, n := range numsLine {
+		num, _ := strconv.Atoi(n)
+		nums = append(nums, num)
+	}
+	return nums
+}
+
+func getInput() ([]int, []*board) {
+	input, _ := ioutil.ReadFile("day4/input.txt")
+	lines := strings.Split(string(input), "\n")
+	nums := getNumLine(lines[0], ",")
+
+	boards := make([]*board, 0)
+	k := 0
+	b := createBoard()
+	for i := 2; i < len(lines); i++ {
+		if k == size {
+			k = 0
+			boards = append(boards, b)
+			b = createBoard()
+			continue
+		}
+		n := getNumLine(lines[i], " ")
+		b.nums = append(b.nums, n)
+		k++
+	}
+	boards = append(boards, b)
+
+	return nums, boards
+}
+
+func getWinner(nums []int, boards []*board, lastWinner bool) (*board, int) {
+	playingBoards := boards[:]
+	for _, num := range nums {
+		for i := 0; i < len(playingBoards); i++ {
+			done := playingBoards[i].mark(num)
+			if done {
+				if !lastWinner || len(playingBoards) == 1 {
+					return playingBoards[i], num
+				}
+				playingBoards = append(playingBoards[:i], playingBoards[i+1:]...)
+				i--
+			}
+		}
+	}
+	return nil, 0
+}
+
+func Part1() {
+	nums, boards := getInput()
+	win, last := getWinner(nums, boards, false)
+	fmt.Printf("day 4, part 1: %d\n", win.sum()*last)
+}
+
+func Part2() {
+	nums, boards := getInput()
+	win, last := getWinner(nums, boards, true)
+	fmt.Printf("day 4, part 2: %d\n", win.sum()*last)
+}
--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/allexysleeps/aoc2021/day3"
+	"github.com/allexysleeps/aoc2021/day4"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	day3.Part1()
-	day3.Part2()
+	day4.Part1()
+	day4.Part2()
 	elapsed := time.Since(start)
 	fmt.Printf("strings time took: %s\n", elapsed)
-
-	start = time.Now()
-	day3.Part1bits()
-	day3.Part2bits()
-	elapsed = time.Since(start)
-	fmt.Printf("bitwise time took: %s\n", elapsed)
 }

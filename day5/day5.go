d2d8087ac582aed0193c86b90591e575ad776bf6
Oleksii Shuliev <alexey.shulev@gmail.com>
2021-12-05 09:44:25 +0100

    day5 geometry

---
+++ day5/day5.go
@@ -0,0 +1,97 @@
+package day5
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func getInput() [][]int {
+	input, _ := ioutil.ReadFile("day5/input.txt")
+	lines := strings.Split(string(input), "\n")
+	points := make([][]int, 0, len(lines))
+	for _, l := range lines {
+		ps := strings.FieldsFunc(l, func(r rune) bool {
+			return !unicode.IsNumber(r)
+		})
+		pi := make([]int, 0, 4)
+		for _, p := range ps {
+			num, _ := strconv.Atoi(p)
+			pi = append(pi, num)
+		}
+		points = append(points, pi)
+	}
+	return points
+}
+
+func minMax(a, b int) (int, int) {
+	if a > b {
+		return b, a
+	}
+	return a, b
+}
+
+func traverse(line []int, hsh map[string]int, incDiagonal bool) {
+	x1, y1, x2, y2 := line[0], line[1], line[2], line[3]
+
+	if x1 == x2 {
+		min, max := minMax(y1, y2)
+		for y := min; y <= max; y++ {
+			hsh[fmt.Sprintf("%d,%d", x1, y)]++
+		}
+		return
+	}
+
+	if y1 == y2 {
+		min, max := minMax(x1, x2)
+		for x := min; x <= max; x++ {
+			hsh[fmt.Sprintf("%d,%d", x, y1)]++
+		}
+		return
+	}
+
+	if !incDiagonal {
+		return
+	}
+
+	if math.Abs(float64(x1-x2)) == math.Abs(float64(y1-y2)) {
+		minX, maxX := minMax(x1, x2)
+		m := (y2 - y1) / (x2 - x1)
+		b := y1 - (m * x1)
+		for x := minX; x <= maxX; x++ {
+			y := m*x + b
+			hsh[fmt.Sprintf("%d,%d", x, y)]++
+		}
+	}
+}
+
+func process(incDiagonal bool) int {
+	lines := getInput()
+	hsh := make(map[string]int)
+	for _, l := range lines {
+		traverse(l, hsh, incDiagonal)
+	}
+	var dPoints int
+	for _, v := range hsh {
+		if v > 1 {
+			dPoints++
+		}
+	}
+	return dPoints
+}
+
+func Part1() {
+	res := process(false)
+	fmt.Printf("day 5, part 1: %d\n", res)
+}
+
+func Part2() {
+	res := process(true)
+	fmt.Printf("day 5, part 2: %d\n", res)
+}
+
+// 6397
+// 22335
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/allexysleeps/aoc2021/day4"
+	"github.com/allexysleeps/aoc2021/day5"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	day4.Part1()
-	day4.Part2()
+	day5.Part1()
+	day5.Part2()
 	elapsed := time.Since(start)
 	fmt.Printf("time took: %s\n", elapsed)
 }

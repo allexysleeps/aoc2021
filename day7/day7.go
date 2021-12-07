aa2bda53f645f8193f042f2e3330428314654c15
Oleksii Shuliev <alexey.shulev@gmail.com>
2021-12-07 10:58:34 +0100

    code style

--- day7/day7.go
+++ day7/day7.go
@@ -8,64 +8,52 @@ import (
 	"strings"
 )
 
-func getInput() []int {
+func getInput() ([]int, int, int) {
 	input, _ := ioutil.ReadFile("day7/input.txt")
 	posS := strings.Split(string(input), ",")
 	posI := make([]int, 0, len(posS))
-	for _, ps := range posS {
+	var min, max int
+	for i, ps := range posS {
 		pi, _ := strconv.Atoi(ps)
+		switch {
+		case i == 0:
+			min, max = pi, pi
+		case pi < min:
+			min = pi
+		case pi > max:
+			max = pi
+		}
 		posI = append(posI, pi)
 	}
-	return posI
+	return posI, min, max
 }
 
-func minMax(pos []int) (int, int) {
-	min, max := pos[0], pos[0]
-	for i := 1; i < len(pos); i++ {
-		if pos[i] > max {
-			max = pos[i]
-			continue
+func minFuel(fuelCons func(target, curr int) int) int {
+	positions, min, max := getInput()
+	minSum := 0
+	for i := min; i <= max; i++ {
+		sum := 0
+		for _, p := range positions {
+			sum += fuelCons(i, p)
 		}
-		if pos[i] < min {
-			min = pos[i]
+		if sum < minSum || i == min {
+			minSum = sum
 		}
 	}
-	return min, max
+	return minSum
 }
 
 func Part1() {
-	positions := getInput()
-	min, max := minMax(positions)
-
-	minSum := 0
-	for i := min; i <= max; i++ {
-		sum := 0
-		for _, p := range positions {
-			sum += int(math.Abs(float64(i - p)))
-		}
-		if sum < minSum || i == min {
-			minSum = sum
-		}
-	}
-
-	fmt.Printf("day 7, part 1: %d\n", minSum)
+	fuel := minFuel(func(target, curr int) int {
+		return int(math.Abs(float64(target - curr)))
+	})
+	fmt.Printf("day 7, part 1: %d\n", fuel)
 }
 
 func Part2() {
-	positions := getInput()
-	min, max := minMax(positions)
-
-	minSum := 0
-	for i := min; i <= max; i++ {
-		sum := 0
-		for _, p := range positions {
-			steps := int(math.Abs(float64(i - p)))
-			sum += steps*(steps+1)/2
-		}
-		if sum < minSum || i == min {
-			minSum = sum
-		}
-	}
-
-	fmt.Printf("day 7, part 2: %d\n", minSum)
+	fuel := minFuel(func(target, curr int) int {
+		steps := int(math.Abs(float64(target - curr)))
+		return steps * (steps + 1) / 2
+	})
+	fmt.Printf("day 7, part 2: %d\n", fuel)
 }

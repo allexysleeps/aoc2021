82ed651a7f37acedae0d959910b378e453817d2c
Oleksii Shuliev <alexey.shulev@gmail.com>
2021-12-13 12:36:43 +0100

    13

---
+++ day13/day13.go
@@ -0,0 +1,107 @@
+package day13
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type point struct {
+	x, y int
+}
+
+type fold struct {
+	axis  string
+	value int
+}
+
+func (p point) fold(f fold) point {
+	if f.axis == "x" {
+		if p.x < f.value {
+			return p
+		}
+		return point{x: 2*f.value - p.x, y: p.y}
+	}
+
+	if p.y < f.value {
+		return p
+	}
+	return point{y: 2*f.value - p.y, x: p.x}
+}
+
+func getInput() ([]point, []fold) {
+	input, _ := ioutil.ReadFile("day13/input.txt")
+	lines := strings.Split(string(input), "\n")
+	points := make([]point, 0)
+	folds := make([]fold, 0)
+	parseFolds := false
+	for _, l := range lines {
+		if l == "" {
+			parseFolds = true
+			continue
+		}
+		if !parseFolds {
+			nums := strings.Split(l, ",")
+			x, _ := strconv.Atoi(nums[0])
+			y, _ := strconv.Atoi(nums[1])
+			points = append(
+				points,
+				point{x: x, y: y},
+			)
+			continue
+		}
+		chunks := strings.Split(l, " ")
+		f := strings.Split(chunks[2], "=")
+		num, _ := strconv.Atoi(f[1])
+		folds = append(
+			folds,
+			fold{axis: f[0], value: num},
+		)
+
+	}
+	return points, folds
+}
+
+func Part1() {
+	points, folds := getInput()
+	foldedPoints := make(map[int]struct{})
+	for _, pt := range points {
+		p := pt.fold(folds[0])
+		idx := p.x*1000 + p.y
+		foldedPoints[idx] = struct{}{}
+	}
+	fmt.Printf("day 13, part 1: %d\n", len(foldedPoints))
+}
+
+func Part2() {
+	points, folds := getInput()
+	foldedPoints := points
+	for _, f := range folds {
+		folded := make([]point, 0)
+		for _, fp := range foldedPoints {
+			p := fp.fold(f)
+			folded = append(folded, p)
+		}
+		foldedPoints = folded
+	}
+
+	ptsMap := make(map[int]struct{})
+	for _, p := range foldedPoints {
+		idx := p.x*100 + p.y
+		ptsMap[idx] = struct{}{}
+	}
+	fmt.Printf("day 13, part 2:\n")
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 50; j++ {
+			idx := j*100 + i
+			_, ok := ptsMap[idx]
+			if ok {
+				fmt.Printf("#")
+				continue
+			}
+			fmt.Printf(".")
+		}
+		fmt.Printf("\n")
+	}
+}
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/allexysleeps/aoc2021/day12"
+	"github.com/allexysleeps/aoc2021/day13"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	day12.Part1()
-	day12.Part2()
+	day13.Part1()
+	day13.Part2()
 	elapsed := time.Since(start)
 	fmt.Printf("time took: %s\n", elapsed)
 }
